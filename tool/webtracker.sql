8120a71a5b767d33c285472d57cc4c729c8a6bea
Mohit.Gupta <mohitguptamt123@gmail.com>
2018-07-02 11:29:10 +0530

    db updated

--- tool/webtracker.sql
+++ tool/webtracker.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2018 at 01:49 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Jul 02, 2018 at 07:57 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -61,15 +61,20 @@ INSERT INTO `botdata` (`visit_id`, `url`, `ip`, `browser`, `browser_version`, `d
 CREATE TABLE `botipinfo` (
   `ip` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
-  `timezone` varchar(255) NOT NULL
+  `timezone` varchar(255) NOT NULL,
+  `isp` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `latitude` varchar(255) NOT NULL,
+  `longitude` varchar(255) NOT NULL,
+  `zip` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `botipinfo`
 --
 
-INSERT INTO `botipinfo` (`ip`, `country`, `timezone`) VALUES
-('::1', '', '');
+INSERT INTO `botipinfo` (`ip`, `country`, `timezone`, `isp`, `city`, `latitude`, `longitude`, `zip`) VALUES
+('::1', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -101,7 +106,10 @@ INSERT INTO `datalog` (`visit_id`, `url`, `ip`, `browser`, `browser_version`, `d
 (1, 'file:///C:/Users/rajat/Documents/webtrackerproject/webTracker/mainScript/index.html', '122.161.193.100', 'Chrome', '67.0.3396', '0000-00-00', '1920 X 1040', 'Windows 10.0.0', '', 'BB-8r9hv7xcP', 'desktop', '15:51:58:280', 'Other'),
 (2, 'file:///C:/Users/rajat/Documents/webtrackerproject/webTracker/mainScript/index.html', '::1', 'Chrome', '67.0.3396', '0000-00-00', '1920 X 1040', 'Windows 10.0.0', '', 'BB-8r9hv7xcP', 'desktop', '16:9:1:571', 'Other'),
 (3, 'file:///C:/Users/rajat/Documents/webtrackerproject/webTracker/mainScript/index.html', '::1', 'Chrome', '67.0.3396', '0000-00-00', '1920 X 1040', 'Windows 10.0.0', '', 'BB-8r9hv7xcP', 'desktop', '16:9:33:467', 'Other'),
-(4, 'file:///C:/Users/rajat/Documents/webtrackerproject/webTracker/mainScript/index.html', '::1', 'Chrome', '67.0.3396', '0000-00-00', '1920 X 1040', 'Windows 10.0.0', 'DIRECT HIT!!', 'BB-8r9hv7xcP', 'desktop', '16:12:3:403', 'Other');
+(4, 'file:///C:/Users/rajat/Documents/webtrackerproject/webTracker/mainScript/index.html', '::1', 'Chrome', '67.0.3396', '0000-00-00', '1920 X 1040', 'Windows 10.0.0', 'DIRECT HIT!!', 'BB-8r9hv7xcP', 'desktop', '16:12:3:403', 'Other'),
+(5, 'file:///D:/GitRepo/webTracker/mainScript/index.html', '::1', 'Chrome', '67.0.3396', '2018-06-29', '1366 X 728', 'Windows 10.0.0', 'DIRECT HIT!!', 'BB-8r9hv7xcP', 'desktop', '17:26:49:822', 'Other'),
+(6, 'file:///D:/GitRepo/webTracker/mainScript/index.html', '::1', 'Chrome', '67.0.3396', '2018-06-29', '1366 X 728', 'Windows 10.0.0', 'DIRECT HIT!!', 'BB-8r9hv7xcP', 'desktop', '17:32:44:748', 'Other'),
+(7, 'file:///D:/GitRepo/webTracker/mainScript/index.html', '::1', 'Chrome', '67.0.3396', '2018-06-29', '1366 X 728', 'Windows 10.0.0', 'DIRECT HIT!!', 'BB-8r9hv7xcP', 'desktop', '17:34:38:558', 'Other');
 
 -- --------------------------------------------------------
 
@@ -172,7 +180,7 @@ ALTER TABLE `botdata`
 -- AUTO_INCREMENT for table `datalog`
 --
 ALTER TABLE `datalog`
-  MODIFY `visit_id` mediumint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `visit_id` mediumint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
